fbc4abf971a6c77326c40c81c91b7099620c23c8
Timothy Baldridge <tbaldridge@gmail.com>
2012-05-02 11:14:37 -0500

    first commit

---
+++ src/microactors/core.clj
@@ -0,0 +1,1 @@
+(ns microactors.core)
---
+++ test/microactors/test/core.clj
@@ -0,0 +1,6 @@
+(ns microactors.test.core
+  (:use [microactors.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
