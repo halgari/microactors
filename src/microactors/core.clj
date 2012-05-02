15eb9c43305437f2d34864135e346ad41426263f
Timothy Baldridge <tbaldridge@gmail.com>
2012-05-02 13:50:46 -0500

    starting work on the core...still wip

--- src/microactors/core.clj
+++ src/microactors/core.clj
@@ -1,1 +1,22 @@
 (ns microactors.core)
+
+(comment
+    
+(defn example-beh [value]
+    (fn [&args]
+        (match [sender :read] (vec (send sender value))
+               [sender :write value] (vec (become example-beh value)
+                                          (send sender value)))))
+=> (de
+
+)
+
+
+(deftype mini-actor [queue beh pid]
+    (queue [msg] (mini-actor. (cons msg queue) beh pid))
+    (msg [] (peek queue)))
+
+(defn run-actor-ctor [actor]
+    (fn [] 
+       (doseq [result (actor)]
+          (cond (instance? result SendCommand)
