0c698af757b2d3402e66c01e75faa4dbe74f3b74
Timothy Baldridge <tbaldridge@gmail.com>
2012-05-02 23:14:13 -0500

    latest work...just need to debug a bit

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject microactors "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.match "0.2.0-alpha9"]])
--- src/microactors/core.clj
+++ src/microactors/core.clj
@@ -1,4 +1,6 @@
-(ns microactors.core)
+(ns microactors.core
+    (:use [clojure.core.match :only [match]])
+    (:import [java.util.concurrent Executors]))
 
 (comment
     
@@ -7,16 +9,52 @@
         (match [sender :read] (vec (send sender value))
                [sender :write value] (vec (become example-beh value)
                                           (send sender value)))))
-=> (de
+
 
 )
 
+(def executor (Executors/newCachedThreadPool))
 
-(deftype mini-actor [queue beh pid]
-    (queue [msg] (mini-actor. (cons msg queue) beh pid))
-    (msg [] (peek queue)))
 
-(defn run-actor-ctor [actor]
-    (fn [] 
-       (doseq [result (actor)]
-          (cond (instance? result SendCommand)
+(defn mini-actor [queue beh]
+    {:queue queue
+     :beh beh})
+
+(defn new-actor [beh]
+    (atom (mini-actor (clojure.lang.PersistentQueue/EMPTY) beh)))
+
+
+(defn merge-become [actor beh args]
+    (mini-actor (:queue @actor) (apply beh args)))
+
+
+(def run-microactor)
+
+(defn exec-send [target args]
+    (let [msg (peek (:queue @target))]
+         (swap! target
+                (fn [old] (assoc old :queue (conj (:queue old) args))))
+         (when-not msg
+             (.execute executor 
+                       (fn [] (run-microactor target))))))
+
+(defn run-microactor [actor]
+    (when-let [msg (peek (:queue @actor))]
+         (doseq [r ((:beh @actor) msg)]
+             (match r 
+                 [:become beh & args]
+                     (merge-become actor beh args)
+                    [:send target & args]
+                     (exec-send target args)))
+         (swap! actor
+               (fn [old]
+                   (assoc old :queue (pop (:queue old)))))
+         (when (peek (:queue @actor))
+             (.execute executor 
+                       (fn [] (run-microactor actor))))))
+
+              
+                      
+
+
+
